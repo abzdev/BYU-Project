706cf527a813f54dc53acd599fa4c0244d91226a
abzdev <76543409+abzdev@users.noreply.github.com>
2021-07-12 15:35:26 -0700

    Added name functionality

--- board.py
+++ board.py
@@ -10,7 +10,7 @@ class Board:
 
         # "board" is a list of 10 strings representing the board (ignore index 0)
         print('   |   |')
-        print(' ' + self.board[7] + ' | ' + self.board[8] + ' | ' + self.board[9])
+        print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
         print('   |   |')
         print('-----------')
         print('   |   |')
@@ -18,7 +18,7 @@ class Board:
         print('   |   |')
         print('-----------')
         print('   |   |')
-        print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
+        print(' ' + self.board[7] + ' | ' + self.board[8] + ' | ' + self.board[9])
         print('   |   |')
 
     def get_board_copy(self):
--- tictactoe.py
+++ tictactoe.py
@@ -11,12 +11,9 @@ def play_again():
     return input().lower().startswith('y')
 
 
-def who_goes_first():
+def who_goes_first(p1, p2):
     # Randomly choose the player who goes first.
-    if random.randint(0, 1) == 0:
-        return 'player1'
-    else:
-        return 'player2'
+    return p1.name if random.randint(0, 1) == 0 else p2.name
 
 
 print('Welcome to Tic Tac Toe!')
@@ -24,10 +21,12 @@ print('Welcome to Tic Tac Toe!')
 while True:
     # Reset the board
     theBoard = Board([' '] * 10)
+    name1 = input("Enter the first player's name: ")
+    name2 = input("Enter the second player's name: ")
     player1Letter, player2Letter = Player.input_1st_player_letter()
-    player1 = Player('Player 1', player1Letter)
-    player2 = Player('Player 2', player2Letter)
-    turn = who_goes_first()
+    player1 = Player(name1, player1Letter)
+    player2 = Player(name2, player2Letter)
+    turn = who_goes_first(player1, player2)
     print(turn + ' will go first.')
     gameIsPlaying = True
 
@@ -41,7 +40,7 @@ while True:
 
             if theBoard.is_winner(player1.letter):
                 theBoard.draw_board()
-                print('Hooray! Player 1 has won the game!')
+                print(f'Hooray! {player1.name} has won the game!')
                 gameIsPlaying = False
             else:
                 if theBoard.is_board_full():
@@ -59,7 +58,7 @@ while True:
 
             if theBoard.is_winner(player2.letter):
                 theBoard.draw_board()
-                print('Hooray! Player 2 has won the game!')
+                print(f'Hooray! {player2.name} has won the game!')
                 gameIsPlaying = False
             else:
                 if theBoard.is_board_full():
